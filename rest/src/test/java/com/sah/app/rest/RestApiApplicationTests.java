eafebe208e0d9ef3850dd8a9aeef76accccdf523
Rajan Sah <83794911+sahrajan@users.noreply.github.com>
2022-05-12 21:17:42 -0400

    Full Stack Project

---
+++ rest/src/test/java/com/sah/app/rest/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sah.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
