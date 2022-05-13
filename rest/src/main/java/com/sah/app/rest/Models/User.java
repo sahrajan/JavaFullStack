51a10ccceb24754b7ec09ff2dd5d81492c9b4d6f
Rajan Sah <83794911+sahrajan@users.noreply.github.com>
2022-05-12 23:42:55 -0400

    project

---
+++ rest/src/main/java/com/sah/app/rest/Models/User.java
@@ -0,0 +1,50 @@
+package com.sah.app.rest.Models;
+
+public class User {
+	private long id;
+	private String firstname;
+	private String lastName;
+	private int age;
+	private String occupation;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getOccupation() {
+		return occupation;
+	}
+	
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+}
